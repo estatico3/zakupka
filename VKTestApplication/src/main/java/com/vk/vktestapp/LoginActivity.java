7b8f2a83ca37d57dd7ce36233c8063c2db2d9bff
ktagintsev <info@webdivision.pro>
2018-03-19 01:23:37 +0500

    Авторизация вконтакте

--- VKTestApplication/src/main/java/com/vk/vktestapp/LoginActivity.java
+++ VKTestApplication/src/main/java/com/vk/vktestapp/LoginActivity.java
@@ -1,17 +1,33 @@
 package com.vk.vktestapp;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.telephony.TelephonyManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKCallback;
 import com.vk.sdk.VKScope;
 import com.vk.sdk.VKSdk;
 import com.vk.sdk.api.VKError;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.net.ssl.HttpsURLConnection;
 
 
 /**
@@ -21,6 +37,7 @@ import com.vk.sdk.api.VKError;
 public class LoginActivity extends FragmentActivity {
 
     private boolean isResumed = false;
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     /**
      * Scope is set of required permissions for your application
@@ -107,11 +124,46 @@ public class LoginActivity extends FragmentActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        final String devicIMEI = telephonyManager.getDeviceId();
         VKCallback<VKAccessToken> callback = new VKCallback<VKAccessToken>() {
             @Override
-            public void onResult(VKAccessToken res) {
+            public void onResult(final VKAccessToken res) {
                 // User passed Authorization
                 startTestActivity();
+                new AsyncTask<Void, String, String>() {
+                    @Override
+                    protected String doInBackground(Void... voids) {
+                        String s = "";
+                        try {
+                            String sig = LoginActivity.md5("expire=" + res.expiresIn + "&mid=" + res.userId + "&secret=" + res.secret + "&sid=" + res.accessToken);
+                            LoginActivity.get("https://api.vmeste.market/android/notifications?device_token=" + devicIMEI + "&expire=" + res.expiresIn + "&mid=" + res.userId + "&secret=" + res.secret + "&sid=" + res.accessToken + "&sig=" + sig);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        return s;
+                    }
+
+                    @Override
+                    protected void onPostExecute(final String result) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                System.out.println(result);
+                            }
+                        });
+                    }
+                }.execute();
             }
 
             @Override
@@ -174,5 +226,49 @@ public class LoginActivity extends FragmentActivity {
             });
             return v;
         }
+    }
+
+    public static String get(String url) throws Exception {
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            int responseCode = con.getResponseCode();
+            System.out.println("\nSending 'GET' request to URL : " + url);
+            System.out.println("Response Code : " + responseCode);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            return response.toString();
+    }
+
+    public static final String md5(final String s) {
+        final String MD5 = "MD5";
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest
+                    .getInstance(MD5);
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuilder hexString = new StringBuilder();
+            for (byte aMessageDigest : messageDigest) {
+                String h = Integer.toHexString(0xFF & aMessageDigest);
+                while (h.length() < 2)
+                    h = "0" + h;
+                hexString.append(h);
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
